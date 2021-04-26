89f54d1e78cf83e50da5864cbef3f74b137efbcd
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-04-26 21:33:25 +0000

    add testing function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMP
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(-1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
