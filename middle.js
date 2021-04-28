99f0f83196c080c9e63c4c510dad6afe0ee50e5c
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-04-28 19:09:14 +0000

    add middle.js to get middle values of an array

---
+++ middle.js
@@ -0,0 +1,45 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (Array.isArray(arrOne) && Array.isArray(arrTwo)) {
+    return arrOne.every((value, index) => value === arrTwo[index]);
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+const middle = function(array) {
+  let middleIndex = array.length / 2;
+
+  if (array.length <= 2) {
+    return [];
+  }
+
+  if (array.length % 2 !== 0) {
+    middleIndex = Math.floor(middleIndex);
+    return array.slice(middleIndex, middleIndex + 1);
+  } else {
+    return array.slice(middleIndex - 1, middleIndex + 1);
+  }
+};
+
+// Test arrays
+const oddArray = [1,2,3,4,5,6,7];
+const oddArrayTwo = [1,2,3,4,5,6,7,8,9];
+const evenArray = [1,2,3,4,];
+const evenArrayTwo = [1,2,3,4,5,6];
+const twoValueArray = [1,2];
+const threeValueArray = [1,2,3];
+
+// assertArray Tests
+assertArraysEqual(middle(oddArray), [4]);
+assertArraysEqual(middle(oddArrayTwo), [5]);
+assertArraysEqual(middle(evenArray), [2,3]);
+assertArraysEqual(middle(evenArrayTwo), [3,4]);
+assertArraysEqual(middle(twoValueArray), []);
+assertArraysEqual(middle(threeValueArray), [2]);
