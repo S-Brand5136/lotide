8406df5d4d55f0c9da41f7c9daae71ad39678eae
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-04-28 22:39:56 +0000

    implement test for primitive values

--- eqObjects.js
+++ eqObjects.js
@@ -9,7 +9,17 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identiacal keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
 
+  for (const key in object1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  
+  return true;
 };
 
 
@@ -22,4 +32,4 @@ eqObjects(ab, ba);
 const abc = { a: "1", b: "2", c: "3"};
 eqObjects(ab, abc);
 
-console.log(assertEqual(eqObjects(ab,ba), true));
+assertEqual(eqObjects(ab,ba), true);
