06973604628676bf7d60582d19d8fc51b7570554
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-04-29 21:59:31 +0000

    add findKey.js with test cases

---
+++ findKey.js
@@ -0,0 +1,79 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  let result = undefined;
+
+  for (const key in object) {
+    if (callback(object[key])) {
+      result = key;
+      break;
+    }
+  }
+
+  return result;
+};
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 4 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+const result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 7 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+const result4 = findKey({
+  "Alice": {grade: 90},
+  "George": {grade: 50},
+  "Bob": {grade: 75},
+  "Charlie": {grade: 60},
+}, x => x.grade === 75);
+
+const result5 = findKey({
+  "Student_One": {
+    firstName: "Alice",
+    lastName: "Balice"
+  },
+  "Student_Two": {
+    firstName: "Bob",
+    lastName: "Chbob"
+  },
+  "Student_Three": {
+    firstName: "Charlie",
+    lastName: "Harlie"
+  },
+  "Student_Four": {
+    firstName: "Denver",
+    lastName: "Henver"
+  }
+}, x => x.firstName[0] === "C");
+
+assertEqual(result1, 'noma');
+assertEqual(result2, 'Ora');
+assertEqual(result3, 'elBulli');
+assertEqual(result4, 'Bob');
+assertEqual(result5, 'Student_Three');
