e61873e16f287ce8189805f16b09b246424e6ee9
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-03 19:52:32 +0000

    add more test cases to assertArraysEqualTest.js

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,3 +2,5 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 // Test cases
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, [3]], [1, 2, [3]]);
