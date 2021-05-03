7462eaf503a57332d4120d22d6627e2f73e67a0f
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-03 19:42:13 +0000

    add test file for tail.js

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = (array) => {
   if (array.length === 1 || array.length === 0) {
     return [];
@@ -14,7 +6,4 @@ const tail = (array) => {
   }
 };
 
-// test cases
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
