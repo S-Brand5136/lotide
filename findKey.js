abb008e55f5f1d931d780c20be2ebad3353cf5ef
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-04 00:57:50 +0000

    add test code for findKeysTest

--- findKey.js
+++ findKey.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const findKey = (object, callback) => {
   let result = undefined;
 
@@ -14,62 +12,3 @@ const findKey = (object, callback) => {
 };
 
 module.exports = findKey;
-
-const result1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2);
-
-const result2 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 4 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2);
-
-const result3 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 7 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 3);
-
-const result4 = findKey({
-  "Alice": {grade: 90},
-  "George": {grade: 50},
-  "Bob": {grade: 75},
-  "Charlie": {grade: 60},
-}, x => x.grade === 75);
-
-const result5 = findKey({
-  "Student_One": {
-    firstName: "Alice",
-    lastName: "Balice"
-  },
-  "Student_Two": {
-    firstName: "Bob",
-    lastName: "Chbob"
-  },
-  "Student_Three": {
-    firstName: "Charlie",
-    lastName: "Harlie"
-  },
-  "Student_Four": {
-    firstName: "Denver",
-    lastName: "Henver"
-  }
-}, x => x.firstName[0] === "Z");
-
-assertEqual(result1, 'noma');
-assertEqual(result2, 'Ora');
-assertEqual(result3, 'elBulli');
-assertEqual(result4, 'Bob');
-assertEqual(result5, undefined);
--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,10 +1,57 @@
 const assert = require('chai').assert;
+const findKey = require('../findKey');
 
-
-describe('', () => {
+describe('#findKey', () => {
   
-  it('', () => {
-    
+  it('Should return "noma"', () => {
+    const dataMap = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    const callback = x => x.stars === 2;
+    const expected = "noma"
+    assert.equal(findKey(dataMap, callback), expected)    
+  });
+
+  it('Should return "Ora"', () => {
+    const dataMap =     {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 4 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    const callback = x => x.stars === 2;
+    const expected = "Ora"
+    assert.equal(findKey(dataMap, callback), expected)    
+  });
+
+  it('Should return undefined if there is no key match', () => {
+    const dataMap =   {
+      "Student_One": {
+        firstName: "Alice",
+        lastName: "Balice"
+      },
+      "Student_Two": {
+        firstName: "Bob",
+        lastName: "Chbob"
+      },
+      "Student_Three": {
+        firstName: "Charlie",
+        lastName: "Harlie"
+      },
+      "Student_Four": {
+        firstName: "Denver",
+        lastName: "Henver"
+      }};
+    const callback = x => x.firstName[0] === "Z";
+    const expected = undefined
+    assert.equal(findKey(dataMap, callback), expected)    
   });
 
 })
