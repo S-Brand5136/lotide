06ca78b5683b59fcb1ca06a7d0c09ab2d3498012
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-04 01:03:01 +0000

    add test code for takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -1,27 +1,3 @@
-
-const eqArrays = function(arrOne, arrTwo) {
-
-  if (Array.isArray(arrOne) && Array.isArray(arrTwo)) {
-  
-    for (let i = 0; i <= arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        return false;
-      }
-    }
-    
-    return true;
-  }
-  return false;
-};
-
-const assertArraysEqual = function(arrOne, arrTwo) {
-  if (eqArrays(arrOne, arrTwo)) {
-    console.log(`✅✅✅ Assertion Passed: ${arrOne} === ${arrTwo}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !== ${arrTwo}`);
-  }
-};
-
 const takeUntil = (array, callback) => {
   const result = [];
 
@@ -37,25 +13,3 @@ const takeUntil = (array, callback) => {
 };
 
 module.exports = takeUntil;
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [1, 2, 5, 7, 2,]);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, ["I've", "been", "to", "Hollywood",]);
-
-console.log('---');
-
-const data3 = [1,2,3,4,5,6,7,8,9,10];
-const results3 = takeUntil(data3, x => x === 7);
-assertArraysEqual(results3, [1,2,3,4,5,6]);
-
-console.log('---');
-
-const data4 = [1, 8, "hello", "world", false, true, "don't", "print"];
-const results4 = takeUntil(data4, x => x === false);
-assertArraysEqual(results4, [1, 8, "hello", "world",]);
--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,10 +1,20 @@
 const assert = require('chai').assert;
+const takeUntil = require('../takeUntil');
 
-
-describe('', () => {
+describe('#takeUntil', () => {
   
-  it('', () => {
-    
+  it('Should return [1, 8, "hello", "world"]', () => {
+    const data = [1, 8, "hello", "world", false, true, "don't", "print"];
+    const callback = x => x === false;
+    const expected = [1,8, 'hello', 'world']
+    assert.deepEqual(takeUntil(data, callback), expected);
+  });
+
+  it(`Should return ["I've", "been", "to", "Hollywood",]`, () => {
+    const data = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+    const callback = x => x === ','
+    const expected = ["I've", "been", "to", "Hollywood",]
+    assert.deepEqual(takeUntil(data, callback), expected);
   });
 
 })
