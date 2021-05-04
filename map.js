98c36846adf30695845664154968055b8bd076b9
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-04 01:06:59 +0000

    add test code for map

--- map.js
+++ map.js
@@ -1,18 +1,3 @@
-const eqArrays = function(arrOne, arrTwo) {
-
-  if (Array.isArray(arrOne) && Array.isArray(arrTwo)) {
-  
-    for (let i = 0; i <= arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        return false;
-      }
-    }
-    
-    return true;
-  }
-  return false;
-};
-
 const map = (array, callback) => {
   const results = [];
 
@@ -24,16 +9,3 @@ const map = (array, callback) => {
 };
 
 module.exports = map;
-
-const words = ["ground", "control", "to", "major", "tom"];
-const numbers = [1, 3, 4, 5, 6, 8];
-
-const results1 = map(words, word => word[0]);
-const results2 = map(numbers, num => num * 2);
-const results3 = map(numbers, number => Math.pow(number, 2));
-const results4 = map(numbers, number => number % 2 === 0 ? 'even' : 'odd');
-
-assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
-assertArraysEqual(results2, [2,6,8,10,12,16]);
-assertArraysEqual(results3, [1,9,16,25,36,64]);
-assertArraysEqual(results4, ['odd', 'odd', 'even', 'odd', 'even', 'even']);
--- test/mapTest.js
+++ test/mapTest.js
@@ -1,10 +1,33 @@
 const assert = require('chai').assert;
+const map = require('../map');
 
-
-describe('', () => {
+describe('#map', () => {
   
-  it('', () => {
-    
+  it(`Should return ['g', 'c', 't', 'm', 't']`, () => {
+    const array = ["ground", "control", "to", "major", "tom"];
+    const expected = ['g', 'c', 't', 'm', 't'];
+    assert.deepEqual(map(array, i => i[0]), expected)
+  });
+
+  it('Should return [1,9,16,25,36,64]', () => {
+    const array = [1, 3, 4, 5, 6, 8];
+    const expected = [1,9,16,25,36,64];
+    assert.deepEqual(map(array, num => Math.pow(num, 2)), expected)
   });
 
 })
+
+
+
+// const words = ["ground", "control", "to", "major", "tom"];
+// const numbers = [1, 3, 4, 5, 6, 8];
+
+// const results1 = map(words, word => word[0]);
+// const results2 = map(numbers, num => num * 2);
+// const results3 = map(numbers, number => Math.pow(number, 2));
+// const results4 = map(numbers, number => number % 2 === 0 ? 'even' : 'odd');
+
+// assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+// assertArraysEqual(results2, [2,6,8,10,12,16]);
+// assertArraysEqual(results3, [1,9,16,25,36,64]);
+// assertArraysEqual(results4, ['odd', 'odd', 'even', 'odd', 'even', 'even']);
