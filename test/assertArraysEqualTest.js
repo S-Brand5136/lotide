da02f9f29d80901bb5a27e2934b56388b3af297b
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-04 00:47:11 +0000

    add test code for eqArrays

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,6 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
-// Test cases
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
-assertArraysEqual([1, 2, [3]], [1, 2, [3]]);
+// // Test cases
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+// assertArraysEqual([1, 2, [3]], [1, 2, [3]]);
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,18 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-// Test cases
+describe('#eqArrays', () => {
+  
+  it('Should return true when 2 equal arrays are given', () => {
+    const array1 = [1,2,3];
+    const array2 = [1,2,3];
+    assert.deepEqual(eqArrays(array1, array2), true);
+  });
 
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+  it('Should return false when 2 arrays are not equal', () => {
+    const array1 = [1,2,3,4];
+    const array2 = [1,2,3];
+    assert.deepEqual(eqArrays(array1, array2), false);
+  });
+  
+  it('Should work with nested arrays', () => {
+    const array1 = [[2, 3, [[4, [5]]], [4]]];
+    const array2 = [[2, 3, [[4, [5]]], [4]]];
+    assert.deepEqual(eqArrays(array1, array2), true);
+  });
 
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-
-assertEqual(eqArrays([[2, 3, [[4, [5]]], [4]]], [[2, 3, [[4, [5]]], [4]]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+})
