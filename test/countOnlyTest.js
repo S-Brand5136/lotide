f2daa6096a818804a42c88f9d5f7ab53e199ea1d
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-04 00:10:16 +0000

    add tests for countOnly

---
+++ test/countOnlyTest.js
@@ -0,0 +1,35 @@
+const assert = require('chai').assert;
+const countOnly = require('../countOnly');
+
+describe('', () => {
+
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+
+  it('Should return {"Jason": 1,"Fang": 2} when { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false } is searched for', () => {
+    const expected = {"Jason": 1, "Fang": 2,};
+    assert.deepEqual(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }), expected);
+  });
+
+  it('Should return {"Karl": 1, "Agouhanna": 1}', () => {
+    const expected = {"Karl": 1, "Agouhanna": 1}
+    assert.deepEqual(countOnly(firstNames, {"Karl": true, "Agouhanna": true}), expected)
+  });
+
+});
+
+// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
